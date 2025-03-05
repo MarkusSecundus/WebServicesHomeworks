60948e0d7b800bdea8b5c8103bba874e0bae536c
MarkusSecundus <kubahronik.g@gmail.com>
2025-03-06 00:49:37 +0100

    add first mock SOAP API

---
+++ 02-soap/workDistributor/src/main/java/tut01/WorkDistributor.java
@@ -0,0 +1,24 @@
+package tut01;
+
+@javax.jws.WebService
+public interface WorkDistributor {
+    
+	@javax.jws.WebMethod
+    public String[] getListOfAssignedPhoneNumbers(String username);   
+	
+
+	@javax.jws.WebMethod
+	public PhoneNumberMetadata getMetadataForPhoneNumber(String phoneNumber);
+
+	@javax.jws.WebMethod
+	public PhoneNumberProgress getPhoneNumberCurrentProgress(String phoneNumber);	
+	@javax.jws.WebMethod
+	public String getEmployeeResponsibleForAssignment(String assignedPhoneNumber);
+
+	@javax.jws.WebMethod
+	public int setPhoneNumberCurrentProgress(String phoneNumber, PhoneNumberProgress progress);
+	
+
+	@javax.jws.WebMethod
+	public int pushNewPhoneNumber(String adminUsername, String adminPassword, PhoneNumberMetadata newPhoneNumber);
+}
